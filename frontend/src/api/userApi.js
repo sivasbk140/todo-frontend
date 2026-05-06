55998a9f139fbb75189dfc0d70186e740d4daac2
SIVABALAKRISHNAN B <sivabalakrishnan140@gmail.com>
2026-05-06 09:18:28 +0530

    Delete frontend directory

--- frontend/src/api/userApi.js
+++
@@ -1,9 +0,0 @@
-import api from "./axiosInstance";
-
-export const getAllUsers = () => {
-  return api.get("/api/Admin");
-};
-
-export const getUserById = (id) => {
-  return api.get(`/api/Admin/${id}`);
-};
